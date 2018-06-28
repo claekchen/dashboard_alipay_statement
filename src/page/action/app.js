3e73a873f3b2b38e246fe3aa5942ca7011a1a1c4
Xi.Chen <Xi.Chen@moodys.com>
2018-06-28 17:27:10 +0800

    change path

--- src/page/action/app.js
+++ src/page/action/app.js
@@ -1,5 +1,5 @@
 import * as types from '../constants/action-type'
-import {findElement, removeIndex} from '../utils'
+import {findElement, removeIndex} from '../../utils'
 export const addFilterAction = (name) => {
     return {
         type: types.ADD_FILTER,
